802802ecbdaf19d8257c2cdf51f2f928b4f39deb
bart3br <95648151+bart3br@users.noreply.github.com>
2023-11-24 04:08:00 +0100

    added personal data (date picker) to fragment right, created menu and color styles

--- app/src/main/java/com/example/exercise03/FragmentRight.kt
+++ app/src/main/java/com/example/exercise03/FragmentRight.kt
@@ -1,10 +1,13 @@
 package com.example.exercise03
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import java.util.Calendar
 
 
 private const val ARG_PARAM1 = "param1"
@@ -41,4 +44,31 @@ class FragmentRight : Fragment() {
                 }
             }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvDate = view.findViewById<TextView>(R.id.tv_date)
+
+        //extracting today's date
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        //creating date picker dialog (pop up calendar)
+        val dateDialog = DatePickerDialog(
+            requireContext(), { _, year, monthOfYear, dayOfMonth ->
+                val formattedDate = "$dayOfMonth-${monthOfYear + 1}-$year"
+                tvDate.text = formattedDate
+            },
+            year,
+            month,
+            day
+        )
+
+        tvDate.setOnClickListener {
+            dateDialog.show()
+        }
+    }
+
 }
