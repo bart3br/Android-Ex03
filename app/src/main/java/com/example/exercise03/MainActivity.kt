fab5aab1fdd23c3a77d1e5a3d8022eef0b5d2dba
bart3br <95648151+bart3br@users.noreply.github.com>
2023-11-24 01:54:54 +0100

    added bottom navigation view to move through fragments, minor layout fixes

--- app/src/main/java/com/example/exercise03/MainActivity.kt
+++ app/src/main/java/com/example/exercise03/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.exercise03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.exercise03.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_container2) as NavHostFragment
+        val navController =navHostFragment.navController
+        val bottomNavMenu: BottomNavigationView = binding.bottomNavigationView
+
+        bottomNavMenu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.fragmentLeft -> {
+                    navController.navigate(R.id.action_global_to_fragLeft)
+                    true
+                }
+                R.id.fragmentCenter -> {
+                    navController.navigate(R.id.action_global_to_fragCenter)
+                    true
+                }
+                R.id.fragmentRight -> {
+                    navController.navigate(R.id.action_global_to_fragRight)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
